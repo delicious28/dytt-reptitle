d38ea244ed5efd46dfd3fac3b9d6fde7bbcb9f5d
luchenwei9266 <a413423382@163.com>
2019-05-27 22:37:50 +0800

    修复获取的下载链接不是真正的下载链接的bug,修复设置page无效的bug

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const limit = require('p-limit')(10);
 const MOVIE_DETAIL_PAGE_URI_SELECTOR = '.co_content8 .ulink';
 const MOVIE_TITLE_SELECTOR = '#header > div > div.bd2 > div.bd3 > div.bd3r > div.co_area2 > div.title_all > h1 > font';
 const MOVIE_IMAGE_SELECTOR = '#Zoom span img';
-const MOVIE_DOWNLOAD_LINK_SELECTOR = '#Zoom > span > table > tbody > tr > td > a';
+// const MOVIE_DOWNLOAD_LINK_SELECTOR = '#Zoom > span > table > tbody > tr > td > a';
+const MOVIE_DOWNLOAD_LINK_SELECTOR = '#Zoom > span > p:nth-child(1) > a';
 const DEFAULT_INCLUDE = ['title', 'imgUrl', 'desc', 'downloadLink', 'descPageLink'];
 const MOVIE_DESC_SELETOR = '#Zoom > span > p:nth-child(1)';
 
@@ -16,10 +17,10 @@ const MOVIE_DESC_SELETOR = '#Zoom > span > p:nth-child(1)';
  * @param {Object} cheerio
  * @returns {Array} movieDetailPageLinks
  */
-function handleGetMovieDetailPageLinks (cheerio) {
-  return cheerio(MOVIE_DETAIL_PAGE_URI_SELECTOR).map((idx, ele) => {
-    return cheerio(ele).attr('href');
-  }).get() || [];
+function handleGetMovieDetailPageLinks(cheerio) {
+    return cheerio(MOVIE_DETAIL_PAGE_URI_SELECTOR).map((idx, ele) => {
+        return cheerio(ele).attr('href');
+    }).get() || [];
 }
 
 /**
@@ -29,40 +30,40 @@ function handleGetMovieDetailPageLinks (cheerio) {
  * @param {Object} config
  * @returns {Array} movieDetails
  */
-function handleGetMovieDetails (cheerio, uri, config) {
-  let { include } = config;
-  if (!include.length) include = DEFAULT_INCLUDE;
-  let result = {};
-  include.forEach(item => {
-    if (item === 'title') {
-      const title = cheerio(MOVIE_TITLE_SELECTOR).text();
-      if (title) result[item] = cheerio(MOVIE_TITLE_SELECTOR).text();
-      else return false;
-    }
-    if (item === 'imgUrl') {
-      const imgUrl = cheerio(MOVIE_IMAGE_SELECTOR).eq(0).map((idx, ele) => {
-        return cheerio(ele).attr('src');
-      }).get()[0];
-      if (imgUrl) result[item] = imgUrl;
-      else return false;
-    }
-    if (item === 'desc') {
-      const desc = cheerio(MOVIE_DESC_SELETOR).text();
-      if (desc) result[item] = desc;
-      else return false;
-    }
-    if (item === 'downloadLink') {
-      const downloadLink = cheerio(MOVIE_DOWNLOAD_LINK_SELECTOR).text();
-      if (downloadLink) result[item] = downloadLink;
-      else return false;
-    }
-    if (item === 'descPageLink') {
-      const descPageLink = uri;
-      if (descPageLink) result[item] = descPageLink;
-      else return false;
-    }
-  });
-  return JSON.stringify(result) === '{}' ? undefined : result;
+function handleGetMovieDetails(cheerio, uri, config) {
+    let { include } = config;
+    if (!include.length) include = DEFAULT_INCLUDE;
+    let result = {};
+    include.forEach(item => {
+        if (item === 'title') {
+            const title = cheerio(MOVIE_TITLE_SELECTOR).text();
+            if (title) result[item] = cheerio(MOVIE_TITLE_SELECTOR).text();
+            else return false;
+        }
+        if (item === 'imgUrl') {
+            const imgUrl = cheerio(MOVIE_IMAGE_SELECTOR).eq(0).map((idx, ele) => {
+                return cheerio(ele).attr('src');
+            }).get()[0];
+            if (imgUrl) result[item] = imgUrl;
+            else return false;
+        }
+        if (item === 'desc') {
+            const desc = cheerio(MOVIE_DESC_SELETOR).text();
+            if (desc) result[item] = desc;
+            else return false;
+        }
+        if (item === 'downloadLink') {
+            const downloadLink = cheerio(MOVIE_DOWNLOAD_LINK_SELECTOR).attr('href');
+            if (downloadLink) result[item] = downloadLink;
+            else return false;
+        }
+        if (item === 'descPageLink') {
+            const descPageLink = uri;
+            if (descPageLink) result[item] = descPageLink;
+            else return false;
+        }
+    });
+    return JSON.stringify(result) === '{}' ? undefined : result;
 }
 
 /**
@@ -72,29 +73,29 @@ function handleGetMovieDetails (cheerio, uri, config) {
  * @returns {Promise} movieDetails
  */
 module.exports = (config) => {
-  config = config || { page: 1, include: DEFAULT_INCLUDE };
-  if (!config.page) throw Error('Config.page is required');
-  let pool = [];
-  for (let i = 0; i < 1; i++) {
-    pool.push(limit(() =>
-      fetch({ ...options(`https://www.dytt8.net/html/gndy/dyzz/list_23_${i + 1}.html`) })
-        .then($ => handleGetMovieDetailPageLinks($))
-        .catch(err => err.toString())
-    ));
-  }
-  return Promise.all(pool)
-    .then(movieDetailPageLinks => flatten(movieDetailPageLinks)) // get solo movie link
-    .then(movieDetailPageLinks => {
-      pool = [];
-      for (let i = 0; i < movieDetailPageLinks.length; i++) {
+    config = config || { page: 1, include: DEFAULT_INCLUDE };
+    if (!config.page) throw Error('Config.page is required');
+    let pool = [];
+    for (let i = 0; i < config.page; i++) {
         pool.push(limit(() =>
-          fetch({ ...options(`https://www.dytt8.net${movieDetailPageLinks[i]}`) })
-            .then($ => handleGetMovieDetails($, `https://www.dytt8.net${movieDetailPageLinks[i]}`, config))
+            fetch({...options(`https://www.dytt8.net/html/gndy/dyzz/list_23_${i + 1}.html`) })
+            .then($ => handleGetMovieDetailPageLinks($))
             .catch(err => err.toString())
         ));
-      }
-      return pool;
-    })
-    .then(pool => Promise.all(pool)) // get solo movie details
-    .then(result => result.filter(v => v));
+    }
+    return Promise.all(pool)
+        .then(movieDetailPageLinks => flatten(movieDetailPageLinks)) // get solo movie link
+        .then(movieDetailPageLinks => {
+            pool = [];
+            for (let i = 0; i < movieDetailPageLinks.length; i++) {
+                pool.push(limit(() =>
+                    fetch({...options(`https://www.dytt8.net${movieDetailPageLinks[i]}`) })
+                    .then($ => handleGetMovieDetails($, `https://www.dytt8.net${movieDetailPageLinks[i]}`, config))
+                    .catch(err => err.toString())
+                ));
+            }
+            return pool;
+        })
+        .then(pool => Promise.all(pool)) // get solo movie details
+        .then(result => result.filter(v => v));
 };
